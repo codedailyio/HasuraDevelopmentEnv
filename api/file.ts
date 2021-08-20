a1f3670c7e9635b24d0b42c6c25ce7cfd77c8f22
Jason Brown <browniefed@gmail.com>
2021-08-20 08:24:28 -0700

    Adds code

--- api/file.ts
+++ api/file.ts
@@ -1,19 +1,62 @@
 import { ApolloServer, gql } from "apollo-server-micro";
 import { send } from "micro";
 import microCors from "micro-cors";
+import { Credentials } from "aws-sdk";
+import S3 from "aws-sdk/clients/s3";
 
 const cors = microCors();
 
 const typeDefs = gql`
+  type FilePayload {
+    filePath: String!
+    url: String!
+  }
+
   type Query {
-    sayHello: String
+    get_file(filePath: String): FilePayload
   }
 `;
 
+export const getS3Access = () => {
+  const access = new Credentials({
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  });
+
+  const s3 = new S3({
+    credentials: access,
+    region: "us-west-2",
+    signatureVersion: "v4",
+  });
+
+  return s3;
+};
+
+const get_s3_image = async ({ filePath }: { filePath: string }) => {
+  const s3 = getS3Access();
+
+  const signedUrlExpireSeconds = 60 * 60 * 2; // 2 hours
+
+  // filePath = derived image path
+  const url = await s3.getSignedUrlPromise("getObject", {
+    Bucket: process.env.AWS_S3_STORAGE_BUCKET,
+    Key: filePath,
+    Expires: signedUrlExpireSeconds,
+  });
+
+  return {
+    url: url,
+  };
+};
+
 const resolvers = {
   Query: {
-    sayHello(parent, args, context) {
-      return "Hello World!";
+    get_file: async (parent, data: { filePath: string }, context) => {
+      const { url } = await get_s3_image({ filePath: data.filePath });
+      return {
+        filePath: data.filePath,
+        url: url,
+      };
     },
   },
 };
---
+++ hasura/migrations/default/1629426874021_create_table_public_files/up.sql
@@ -0,0 +1,1 @@
+CREATE TABLE "public"."files" ("id" bigserial NOT NULL, "filePath" text NOT NULL, PRIMARY KEY ("id") );
